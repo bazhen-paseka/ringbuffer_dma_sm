3c6c85ae27e5a557da75fbc68e7c993b16c3e3ca
Бажен Левковець <bazhen.paseka@gmail.com>
2019-01-07 18:17:32 +0200

    UART read work

--- ringbuffer_dma_sm.c
+++ ringbuffer_dma_sm.c
@@ -8,8 +8,7 @@
 	extern UART_HandleTypeDef huart1;
 	extern UART_HandleTypeDef huart3;
 
-	#define RX_BUFFER_SIZE 30
-	uint8_t rx_circular_buffer2[RX_BUFFER_SIZE];
+	#define RX_BUFFER_SIZE 256
 
 	extern DMA_HandleTypeDef hdma_usart3_rx;
 	RingBuffer_DMA rx_buffer3;
@@ -24,6 +23,10 @@
 	#define WIFI_PASS			"27051329"
 
 	char wifi_cmd[512];
+
+	uint32_t rx_count = 0;
+	uint8_t icmd = 0;
+	char cmd[512];
 
 //*************************************************************************
 
@@ -76,7 +79,7 @@ void RingBuffer_DMA_Init(RingBuffer_DMA * buffer, DMA_HandleTypeDef * hdma, uint
 void RingBuffer_DMA_Connect(void)
 {
 	/* Init RingBuffer_DMA object */
-	  RingBuffer_DMA_Init(&rx_buffer3, &hdma_usart3_rx, rx_circular_buffer3,RX_BUFFER_SIZE);
+	  RingBuffer_DMA_Init(&rx_buffer3, &hdma_usart3_rx, rx_circular_buffer3, RX_BUFFER_SIZE);
 	  HAL_UART_Receive_DMA(&huart3, rx_circular_buffer3, RX_BUFFER_SIZE);
 
 
@@ -135,35 +138,38 @@ void RingBuffer_DMA_Connect(void)
 }
 //*************************************************************************
 
-//=================================================================
 void UART_Read (void)
  {
-	uint32_t rx_count = 0;
-	uint8_t icmd = 0;
-	char cmd[512];
-
 	/* Check number of bytes in RingBuffer */
 	rx_count = RingBuffer_DMA_Count(&rx_buffer3);
+	sprintf(wifi_cmd,"DMA_count= %d\r\n", (int)rx_count);
+	HAL_UART_Transmit(&huart1, (uint8_t *)wifi_cmd, strlen(wifi_cmd), 100);
+
 	/* Process each byte individually */
-	while (rx_count--) {
-		/* Read out one byte from RingBuffer */
-		uint8_t b = RingBuffer_DMA_GetByte(&rx_buffer3);
-		if (b == '\n') { /* If \n process command */
+	while (rx_count--)
+	{
+		uint8_t b = RingBuffer_DMA_GetByte(&rx_buffer3);	/* Read out one byte from RingBuffer */
+		if (b == '\n')
+		{ /* If \n process command */
 			/* Terminate string with \0 */
 			cmd[icmd] = 0;
 			icmd = 0;
 			/* Display received messages */
-			//LCD_Printf("%s\n", cmd);
+
 			sprintf(wifi_cmd,"%s\r\n", cmd);
 			HAL_UART_Transmit(&huart1, (uint8_t *)wifi_cmd, strlen(wifi_cmd), 100);
 		  	HAL_Delay(100);
-		} else if (b == '\r') { /* If \r skip */
-			continue;
-		} else { /* If regular character, put it into cmd[] */
-			cmd[icmd++ % 512] = b;
+		}
+		else if (b == '\r')
+		{
+			continue;	/* If \r skip */
+		}
+		else
+		{
+			cmd[icmd++ % 512] = b;	/* If regular character, put it into cmd[] */
 		}
 	}
- }// void UART_Read1 (void)
+ }	// void UART_Read(void)
 //======================================================================
 
 void RingBuffer_DMA_Main(void)
@@ -191,6 +197,6 @@ void RingBuffer_DMA_Main(void)
 	HAL_Delay(100);
 	UART_Read();
 
-	HAL_Delay(20000);
+	HAL_Delay(40000);
 }
 //*************************************************************************
